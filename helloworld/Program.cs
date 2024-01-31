2bde8de825239443329727cc4849d7583f591d04
Danil Zvyagintsev <119746536+danzvyagintsev@users.noreply.github.com>
2024-02-01 01:12:36 +0600

    Update Program.cs

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 string firstFriend = "Maria";
@@ -6,8 +6,8 @@ string secondFriend = "Scott";
 string thirdFriend = "   John";
 string fourthFriend = "    Brian   ";
 
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend}.");
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}."); //String Interpolation
 
-Console.WriteLine("My friend is " + firstFriend + " and " + secondFriend + ".");
+Console.WriteLine("My friends are " + firstFriend + " and " + secondFriend + "."); //String Concatenation
 
-Console.WriteLine($"{thirdFriend.TrimStart()} and {fourthFriend.Trim()} are not my friends.");
+Console.WriteLine($"{thirdFriend.TrimStart()} and {fourthFriend.Trim()} are not my friends."); //Trim() Method
