a666af6361ce63246eb90219bfc18d4ef8d0d68c
Danil Zvyagintsev <119746536+danzvyagintsev@users.noreply.github.com>
2024-02-02 01:21:51 +0600

    Update numbers3.cs

--- Code Files/numbers3.cs
+++ Code Files/numbers3.cs
@@ -1,4 +1,4 @@
 decimal a = 42.1M; //explicit type
 decimal b = 38.2M;
 decimal c = a + b;
-Console.WriteLine(c); //returns 80.3
+Console.WriteLine("The answer for this is " + c); //returns The answer for this is 80.3
