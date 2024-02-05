59f39782f5ead22d6b8e954f62e4acc335a380a6
Danil Zvyagintsev <119746536+danzvyagintsev@users.noreply.github.com>
2024-02-05 22:58:03 +0600

    Delete Code Files/void.cs

--- Code Files/void.cs
+++
@@ -1,8 +0,0 @@
-//If a method doesn’t receive input, use empty parentheses. If it doesn’t return anything, use the void keyword:
-
-using System;
-SayHello();
-void SayHello()
-{
- Console.WriteLine ("Hello, world");
-}
